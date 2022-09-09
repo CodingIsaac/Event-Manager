f3b99eb39118e39615e602265f557144ad050d4c
piio-zicko <isaacijuo@gmail.com>
2022-09-09 14:53:39 +0000

    Updated Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 import AddEvent from "./components/Users/AddEvent";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+ const [usersList, setUsersList] = useState([])
+
+ const addUserEventHandler = (uTitle, uDescription) => {
+  setUsersList((prevEventList) => {
+    return[...prevEventList, {title: uTitle, description: uDescription, id:Math.random().toString } ];
+  } );
+
+ }
+
   return (
     <div>
-      <AddEvent />
+      <AddEvent onAddEvent={addUserEventHandler} />
+      <UsersList users={usersList} />
 
     </div>
   );
--- src/components/Users/AddEvent.js
+++ src/components/Users/AddEvent.js
@@ -1,41 +1,54 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import  classes from "./AddEvent.module.css"
+import classes from "./AddEvent.module.css";
 import Button from "../UI/Button";
 
+const AddEvent = (props) => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredDescription, setEnteredDescription] = useState("");
 
-const AddEvent = props => {
-   const [enteredTitle, setEnteredTitle] = useState('');
-   const [enteredDescription, setEnteredDescription] = useState('');
-
-    
-        const addEventHandler = (event) => {
-            event.preventDefault();
-            console.log(enteredTitle, enteredDescription)
-        };
-
-        const titleChangeHandler = (event) => {
-            setEnteredTitle(event.target.value);
-
-        } 
-
-        const descriptionChangeHandler = (event) => {
-            setEnteredDescription(event.target.value);
-
-        } 
+  const addEventHandler = (event) => {
+    event.preventDefault();
+    if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0) {
+        return;
+    }
 
     
 
-    return (
-        <Card className = {classes.input} >
-    <form onSubmit={addEventHandler} >
-        <label htmlFor="title" >Title</label>
-        <input id="title" type="text" onChange={titleChangeHandler} />
-        <label htmlFor="title" >Description</label>
-        <input id="title" type="text" onChange={descriptionChangeHandler} />
+    props.onAddEvent(enteredTitle, enteredDescription);
+    setEnteredTitle("");
+    setEnteredDescription("");
+  };
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const descriptionChangeHandler = (event) => {
+    setEnteredDescription(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addEventHandler}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+        />
+        <label htmlFor="title">Description</label>
+        <input
+          id="title"
+          type="text"
+          value={enteredDescription}
+          onChange={descriptionChangeHandler}
+        />
         <Button type="submit">Add Event</Button>
-    </form>
+      </form>
     </Card>
-)}
+  );
+};
 
-export default AddEvent
+export default AddEvent;
